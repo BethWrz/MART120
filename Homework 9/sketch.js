4a0a6e6b1d324e34797108c710ecd7de47b05b2a
BethWrz <elizabeth.wrzesinski@umconnect.umt.edu>
2021-11-01 20:20:33 -0600

    Homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,76 @@
+// JavaScript Document
+		function setup()
+		{
+		createCanvas (500,600);
+		}
+
+		function draw()
+		{
+		background(167,192,242);
+			
+
+			strokeWeight(0);
+			fill(217,167,139);
+		ellipse(340,145,70,60);
+		ellipse(160,145,70,60);
+			
+			fill(38,21,15);
+		rect(160,50,180,100);
+		rect(160,170,180,100);
+			
+			fill(242,196,155);
+		circle(250, 150, 200);
+		rect(170,150,150,200);
+			
+			fill(217,167,139);
+		triangle(230,200,248,180,240,140);
+			
+			fill(255,255,255);
+		circle(295,150,34);
+		circle(195,150,34);
+			
+			fill(38,21,15);
+		circle(295,150,15);
+		circle(195,150,15);
+		rect(188,50,155,60);
+		rect(155,50,33,60);
+			rect(155,100,33,60);
+			rect(155,75,33,60);
+		rect(300,50,45,60);
+			rect(300,100,45,60);
+			
+			
+			fill(0,0,0)
+		circle(295,150,5);
+		circle(195,150,5);
+			
+			fill(140,89,59)
+		circle(300,145,5);
+		circle(190,145,5);
+			
+			fill(70,94,115)
+		rect(230,410,100,200);
+		rect(180,410,100,200);
+
+			fill(115,2,12);
+		square(150,270,200);
+		rect(100,290,100,200);
+		rect(300,290,100,200);	
+			
+			strokeWeight(3);
+			line(100, 300, 400, 300);
+			line(100, 350, 400, 350);
+			line(100, 400, 400, 400);
+			line(100, 450, 400, 450);
+			
+			
+			strokeWeight (50)
+			point(210, 600);
+			point(300, 600);
+			
+			fill(61,89,20)
+			
+			text('Self Portrait of Me (Beth)!', 10, 30);
+			text('Coded by Beth Wrzesinski', 350, 550);
+			
+		}
